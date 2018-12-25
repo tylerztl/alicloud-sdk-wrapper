9995818cf40e5798563f2020c19a81f8f482976a
fanjiahe <fanjiahe@ziggurat.cn>
2018-12-25 08:42:02 +0800

    add shell scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Download dependent packages
+
+go get github.com/astaxie/beego
+go get github.com/aliyun/alibaba-cloud-sdk-go
+
+
+# Download bee command tool
+
+go get -u github.com/beego/bee
