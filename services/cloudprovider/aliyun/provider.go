ba8db8472ada497b9546c6f913b5adbe399bcbd9
fanjiahe <fanjiahe@ziggurat.cn>
2019-01-17 17:21:19 +0800

    add the simple methods

--- services/cloudprovider/aliyun/provider.go
+++ services/cloudprovider/aliyun/provider.go
@@ -1,14 +1,38 @@
 package aliyun
 
+import (
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+)
 
 type CloudProvider struct {
 
 }
 
-func (provider *CloudProvider) CreateInstance() {
-
+func (provider *CloudProvider) CreateInstance() interface{} {
+	createInstanceRequest := &ecs.CreateInstanceRequest{}
+	client := provider.GetClient()
+	response, err := client.CreateInstance(createInstanceRequest)
+	if err == nil {
+		return response
+	}else{
+		return nil
+	}
 }
 
 func (provider *CloudProvider) RunInstance() {
+	client := provider.GetClient()
+	runInstancesRequest := ecs.RunInstancesRequest{}
+	client.RunInstances(&runInstancesRequest)
+}
 
+func (provider *CloudProvider) GetClient()  *ecs.Client {
+	regionId := ""
+	accessKeyId := ""
+	accessKeySecret := ""
+	client, err := ecs.NewClientWithAccessKey(regionId, accessKeyId,accessKeySecret)
+	if err == nil {
+		return client
+	}else {
+		return nil;
+	}
 }
