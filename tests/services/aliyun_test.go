0c178e6531c8c7c4a00353a482cb9632c66fe1e6
fanjiahe <fanjiahe@ziggurat.cn>
2019-01-18 15:25:50 +0800

    refine codes

---
+++ tests/services/aliyun_test.go
@@ -0,0 +1,68 @@
+package services
+
+import (
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
+	"fmt"
+	"testing"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/utils"
+)
+
+func GetClient() *ecs.Client {
+	//var regionId string = "cn-hangzhou"
+	var regionId string = "us-west-1"
+	var accessKeyId string = "76PKIccqlFZERf7X"
+	var accessKeySecret string = "6QHPmxyEf80WqUNinteE1WWWqoz8IW"
+
+	client, err := ecs.NewClientWithAccessKey(regionId, accessKeyId, accessKeySecret)
+
+	if err == nil {
+		return client
+	} else {
+		fmt.Println(">>> encounter errors")
+		return nil
+	}
+}
+
+func TestAliyunClient(t *testing.T) {
+
+	client := GetClient()
+
+	request := ecs.CreateDescribeInstanceAttributeRequest()
+	request.InstanceId = "i-rj99oa9orptlgqdhyftu"
+	response, err := client.DescribeInstanceAttribute(request)
+
+	if err != nil {
+		fmt.Println(">>> encounter request errors")
+		t.Error(err)
+	}
+	t.Log(response.GetHttpContentString())
+}
+
+func TestCreateInstance(t *testing.T) {
+	client := GetClient()
+	request := ecs.CreateCreateInstanceRequest()
+	request.ImageId = "ubuntu_16_0402_64_20G_alibase_20180409.vhd"
+	request.InstanceName = "MyInstance"
+	//request.SecurityGroupId = "<your-security-group-id>"
+	request.InstanceType = "ecs.sn2ne.large"
+	//request.InstanceType = "ecs.t1.small"
+	request.ClientToken = utils.GetUUIDV4()
+	response, err := client.CreateInstance(request)
+	if err != nil {
+		fmt.Println(">>> encounter request errors")
+		t.Error(err)
+	}
+	t.Log(response.GetHttpContentString())
+}
+
+func TestDeleteInstance(t *testing.T) {
+	client := GetClient()
+	request := ecs.CreateDeleteInstanceRequest()
+	request.InstanceId = "i-rj99oa9orptlgqdhyftu"
+	response, err := client.DeleteInstance(request)
+	if err != nil {
+		fmt.Println(">>> encounter request errors")
+		t.Error(err)
+	}
+	t.Log(response.GetHttpContentString())
+}
--- tests/services/provider_test.go
+++ tests/services/provider_test.go
@@ -4,45 +4,8 @@ import (
 	"testing"
 	"zig-cloud/services"
 	"zig-cloud/services/cloudprovider/aliyun"
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
-	"fmt"
 )
 
-
-func GetClient() *ecs.Client {
-	//var regionId string = "cn-hangzhou"
-	var regionId string = "us-west-1"
-	var accessKeyId string = "76PKIccqlFZERf7X"
-	var accessKeySecret string = "6QHPmxyEf80WqUNinteE1WWWqoz8IW"
-
-	client, err := ecs.NewClientWithAccessKey(regionId, accessKeyId, accessKeySecret)
-
-	if err == nil {
-		return client
-	} else {
-		fmt.Println(">>> encounter errors")
-		return nil
-	}
-}
-
-func TestHelloWorld(t *testing.T) {
-	t.Log("Hello World")
-}
-
-func TestAliyunClient(t *testing.T) {
-
-	client := GetClient()
-
-	request := ecs.CreateDescribeInstanceAttributeRequest()
-	request.InstanceId = "i-rj99oa9orptlgqdhyftu"
-	response, err := client.DescribeInstanceAttribute(request)
-
-	if err != nil {
-		fmt.Println(">>> encounter request errors")
-		t.Error(err)
-	}
-	t.Log(response.GetHttpContentString())
-}
 
 func TestAliyunCreateInstance(t *testing.T) {
 	var provider services.Provider
