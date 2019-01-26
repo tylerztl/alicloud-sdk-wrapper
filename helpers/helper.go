456dc6c1f9f0cf3959dc5505a0fb8292f0974da6
zhangtailin <zhangtailin@ziggurat.cn>
2019-01-26 21:35:12 +0800

    add instance stop and delete

--- helpers/helper.go
+++ helpers/helper.go
@@ -18,10 +18,6 @@ func GenerateSecurityGroupName() string {
 	return fmt.Sprintf("%s"+commons.SeparatorHype+"%s", commons.AliCloudSecurityGroupName, GenerateRandomString())
 }
 
-func GenerateSecurityGroupRuleName() string {
-	return fmt.Sprintf("%s"+commons.SeparatorHype+"%s", commons.AliCloudSecurityGroupRuleName, GenerateRandomString())
-}
-
 func GenerateRandomString() string {
 	rand.Seed(time.Now().UnixNano())
 	chars := []rune("abcdefghijklmnopqrstuvwxyz" + "0123456789")
@@ -36,4 +32,19 @@ func GenerateRandomString() string {
 
 func GetTimeoutMessage(product, status string) string {
 	return fmt.Sprintf("Waitting for %s %s is timeout.", product, status)
+}
+
+func ConvertListToJsonString(configured []string) string {
+	if len(configured) < 1 {
+		return ""
+	}
+	result := "["
+	for i, v := range configured {
+		result += "\"" + v + "\""
+		if i < len(configured)-1 {
+			result += ","
+		}
+	}
+	result += "]"
+	return result
 }
